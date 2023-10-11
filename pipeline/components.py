749c0fba2d54dc5fe6af36d503e537220ec68450
AfolabiAwonuga <afolabiawonuga@gmail.com>
2023-10-11 02:04:12 +0100

    Updates

--- pipeline/components.py
+++ pipeline/components.py
@@ -11,7 +11,7 @@ from clearml import PipelineDecorator
 def get_data_comp(
     name: str
 ):
-    from preprocess import get_data
+    from fitness_package.preprocess import get_data
     data_path = get_data(name)
     return data_path
 
@@ -27,7 +27,7 @@ def tranform_comp(
     data_path: str
 ):
     import pandas as pd
-    from preprocess import transform
+    from fitness_package.preprocess import transform
     data = pd.read_csv(data_path)
     transformed_data = transform(data, 'day_of_week', 'days_before')
     return transformed_data
@@ -43,7 +43,7 @@ def tranform_comp(
 def encode_comp(
     data: pd.DataFrame
 ):
-    from preprocess import encode
+    from fitness_package.preprocess import encode
     from sklearn import set_config
     set_config(transform_output='pandas')
     encoded_data = encode(data, 'day_of_week', 'time', 'category', 'booking_id')
@@ -67,7 +67,7 @@ def upload_dataset_comp(
     parent: str=None
 
 ):
-    from preprocess import upload_data
+    from fitness_package.preprocess import upload_data
     upload_data(
         path,
         name,
